b333af936935691090138f347712aec2d318a7ec
Gus <gustavoocanto@gmail.com>
2021-12-16 16:11:56 +0800

    work on the app abstraction

--- database/connection/connection.go
+++ database/connection/connection.go
@@ -2,21 +2,27 @@ package connection
 
 import (
 	"fmt"
+	"github.com/voyago/environment"
 	"gorm.io/gorm"
 )
 
 type Connection struct {
-	db *gorm.DB
+	db     *gorm.DB
+	driver *gorm.Dialector
 }
 
-func MakeConnection() (Connection, error) {
-	db, err := gorm.Open(driver())
+func Make(env environment.Env) (Connection, error) {
+	driver := MakeDriver(env)
+	db, err := gorm.Open(driver.dialector, &gorm.Config{})
 
 	if err != nil {
-		return Connection{}, fmt.Errorf("The database connection failed: %v\n", err)
+		return Connection{}, fmt.Errorf("The database [%s] connection failed: %v\n", env.Get("DATABASE_NAME"), err)
 	}
 
-	return Connection{
-		db: db,
-	}, nil
+	conn := Connection{
+		db:     db,
+		driver: &driver.dialector,
+	}
+
+	return conn, nil
 }
