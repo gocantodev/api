61641d0a839cc5661ab991500727b6a2654953ac
Gustavo Ocanto <gustavoocanto@gmail.com>
2022-12-26 16:14:35 +0800

    create response helper

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,34 +6,45 @@ import (
 	"github.com/gocantodev/server/config"
 	log "github.com/sirupsen/logrus"
 	"net/http"
+	"os"
 )
 
-type Time struct {
-	CurrentTime string `json:"current_time"`
+type ResponseError struct {
+	Message string `json:"message"`
+}
+
+type Response struct {
+	Configuration config.Config `json:"configuration"`
+	Seed          string        `json:"seed"`
+	SeedName      string        `json:"seedName"`
 }
 
 func main() {
-	configuration, err := config.Make()
-
-	if err != nil {
-		fmt.Println("error: %w", err)
-		return
-	}
-
-	fmt.Println(configuration)
-
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", getTime)
+	mux.HandleFunc("/config", getConfig)
 
 	fmt.Println("Server is running at 127.0.0.1:3000")
-	err = http.ListenAndServe(":3000", mux)
+	err := http.ListenAndServe(":3000", mux)
 	log.Fatal(err)
 }
 
-func getTime(w http.ResponseWriter, r *http.Request) {
-	currentTime := []Time{
-		{CurrentTime: http.TimeFormat},
+func getConfig(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	configuration, err := config.Make()
+
+	if err != nil {
+		w.WriteHeader(500)
+		response := ResponseError{Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
 	}
 
-	json.NewEncoder(w).Encode(currentTime)
+	response := Response{
+		Configuration: *configuration,
+		SeedName:      (*configuration).App.Env,
+		Seed:          os.Getenv((*configuration).App.Env),
+	}
+
+	json.NewEncoder(w).Encode(response)
 }
--- config/config.go
+++ config/config.go
@@ -1,25 +1,25 @@
 package config
 
 type App struct {
-	Env      string `yaml:"env"`
-	Name     string `yaml:"name"`
-	LogLevel string `yaml:"log_level"`
+	Env      string `yaml:"env" json:"env"`
+	Name     string `yaml:"name" json:"name"`
+	LogLevel string `yaml:"log_level" yaml:"logLevel"`
 }
 
 type Database struct {
-	ImageVersion string `yaml:"image_version"`
-	Version      string `yaml:"version"`
-	Name         string `yaml:"name"`
-	User         string `yaml:"user"`
-	Password     string `yaml:"password"`
-	Host         string `yaml:"host"`
-	Url          string `yaml:"url"`
-	MaxLifetime  int    `yaml:"max_lifetime"`
-	MaxOpenCons  int    `yaml:"max_open_cons"`
-	MaxIdleCons  int    `yaml:"max_idle_cons"`
+	ImageVersion string `yaml:"image_version" json:"imageVersion"`
+	Version      string `yaml:"version" json:"version"`
+	Name         string `yaml:"name" json:"name"`
+	User         string `yaml:"user" json:"user"`
+	Password     string `yaml:"password" json:"password"`
+	Host         string `yaml:"host" json:"host"`
+	Url          string `yaml:"url" json:"url"`
+	MaxLifetime  int    `yaml:"max_lifetime" json:"maxLifetime"`
+	MaxOpenCons  int    `yaml:"max_open_cons" json:"maxOpenCons"`
+	MaxIdleCons  int    `yaml:"max_idle_cons" json:"maxIdleCons"`
 }
 
 type Config struct {
-	App      App      `yaml:"app"`
-	Database Database `yaml:"database"`
+	App      App      `yaml:"app" json:"app"`
+	Database Database `yaml:"database" json:"database"`
 }
